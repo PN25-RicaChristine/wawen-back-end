cc987243fbdb1f5a483ec732ebfceb561bc33626
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2020-10-20 17:28:38 +0800

    ,,

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call([
-            //  AccountTableSeeder::class,
+             AccountTableSeeder::class,
              ProductTableSeeder::class
          ]);
     }
